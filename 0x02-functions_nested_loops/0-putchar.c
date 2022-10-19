27aa01877fb2310b3b5309b8d6c7491c029eb49f
kwekku <rowland.kay.jones@gmail.com>
2022-10-18 19:58:10 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - prints _putchar, followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int main(void)
+	{
+		int i;
+		char c[] = "_putchar";
+
+		for (i = 0; i <= 7; i++)
+		{
+			putchar(c[i]);
+		}
+
+		putchar('\n');
+
+		return (0);
+	}
+}
