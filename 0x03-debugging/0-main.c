7e84a0448a3ae7fa712fb37b78d04621b2e3c186
kwekku <rowland.kay.jones@gmail.com>
2022-10-27 06:15:12 -0700

    Task 0 update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
 #include "main.h"
+
 /**
  * main - tests the function that prints if integer is positive or negative
- *
  * Return: 0
- *
  */
-int positive_or_negative(int);
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
