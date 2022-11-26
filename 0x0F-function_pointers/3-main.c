c4b44775345a224dd553efa50240b62ed322409d
kwekku <rowland.kay.jones@gmail.com>
2022-11-25 22:15:21 -0800

    function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point of program
+ * @argc: size of array argv and number of command line arguments
+ * @argv: An array of size argc
+ * Return: Always successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (get_op_func(argv[2]) == NULL)
+		printf("Error\n"), exit(99);
+	if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
+		printf("Error\n"), exit(100);
+	printf("%d\n", (get_op_func(argv[2])(num1, num2)));
+	return (0);
+}
