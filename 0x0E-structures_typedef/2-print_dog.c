7e81524be10e009749cba9c531caa047fab60a71
kwekku <rowland.kay.jones@gmail.com>
2022-11-26 07:44:50 -0800

    struc_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - print struct dog
+ * @d: pointer to struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+		printf("Age: %f\n", d->age);
+		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
+	}
+}
